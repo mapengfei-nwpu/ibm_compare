b555e1d5c2c4e269ff14c16d4bf437e1889b49bd
mapengfei-nwpu <33214870+mapengfei-nwpu@users.noreply.github.com>
2020-05-05 16:14:24 +0800

    wrap the solver into a function

--- SAV方法和其他方法的比较/投影方法的比较/projection.py
+++ SAV方法和其他方法的比较/投影方法的比较/projection.py
@@ -2,103 +2,100 @@
 from dolfin import *
 from exact_solutions import solutions
 
-mesh = RectangleMesh(Point(0, 0), Point(1, 1), 8, 8)
-
-solution = solutions[0]
-
 # Set dolfin parameters
 prec = "amg" if has_krylov_solver_preconditioner("amg") else "default"
 parameters['krylov_solver']['nonzero_initial_guess'] = True
+parameters["std_out_all_processes"] = False
 
-f = Expression((solution["fx"], solution["fy"]), degree=2, t=0)
-p_exact = Expression(solution["p"], degree=2, t=0)
-u_exact = Expression((solution["ux"], solution["uy"]), degree=2, t=0)
 
-# Define function spaces (P2-P1)
-V = VectorFunctionSpace(mesh, "Lagrange", 2)
-Q = FunctionSpace(mesh, "Lagrange", 1)
+def PrejectionSolve(n, solution):
+    mesh = RectangleMesh(Point(0, 0), Point(1, 1), 8, 8)
+    f = Expression((solution["fx"], solution["fy"]), degree=2, t=0)
+    p_exact = Expression(solution["p"], degree=2, t=0)
+    u_exact = Expression((solution["ux"], solution["uy"]), degree=2, t=0)
 
-# Define trial and test functions
-u = TrialFunction(V)
-p = TrialFunction(Q)
-v = TestFunction(V)
-q = TestFunction(Q)
+    # Define function spaces (P2-P1)
+    V = VectorFunctionSpace(mesh, "Lagrange", 2)
+    Q = FunctionSpace(mesh, "Lagrange", 1)
 
-# Set parameter values
-dt = 0.0001
-T = 0.1
-nu = 0.01
+    # Define trial and test functions
+    u = TrialFunction(V)
+    p = TrialFunction(Q)
+    v = TestFunction(V)
+    q = TestFunction(Q)
 
-# Define boundary conditions
-bcu = [DirichletBC(V, u_exact, "on_boundary")]
-bcp = [DirichletBC(
-    Q, p_exact, "x[0] < DOLFIN_EPS && x[1] < DOLFIN_EPS", "pointwise")]
+    # Set parameter values
+    dt = 1e-5
+    T = 0.1
+    nu = 0.01
 
-# Create functions
-u0 = Function(V)
-u1 = Function(V)
-p1 = Function(Q)
+    # Define boundary conditions
+    bcu = [DirichletBC(V, u_exact, "on_boundary")]
+    bcp = [DirichletBC(
+        Q, p_exact, "x[0] < DOLFIN_EPS && x[1] < DOLFIN_EPS", "pointwise")]
 
-# Define coefficients
-k = Constant(dt)
+    # Create functions
+    u0 = Function(V)
+    u1 = Function(V)
+    p1 = Function(Q)
 
-# Tentative velocity step
-F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
-    nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
-a1 = lhs(F1)
-L1 = rhs(F1)
+    # Define coefficients
+    k = Constant(dt)
 
-# Pressure update
-a2 = inner(grad(p), grad(q))*dx
-L2 = -(1/k)*div(u1)*q*dx
+    # Tentative velocity step
+    F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
+        nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
+    a1 = lhs(F1)
+    L1 = rhs(F1)
 
-# Velocity update
-a3 = inner(u, v)*dx
-L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
+    # Pressure update
+    a2 = inner(grad(p), grad(q))*dx
+    L2 = -(1/k)*div(u1)*q*dx
 
-# Assemble matrices
-A1 = assemble(a1)
-A2 = assemble(a2)
-A3 = assemble(a3)
+    # Velocity update
+    a3 = inner(u, v)*dx
+    L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
 
-# Create files for storing solution
-ufile = File("results/velocity.pvd")
-pfile = File("results/pressure.pvd")
+    # Assemble matrices
+    A1 = assemble(a1)
+    A2 = assemble(a2)
+    A3 = assemble(a3)
 
-# Time-stepping
-t = dt
-while t < T + DOLFIN_EPS:
+    # Time-stepping
+    t = dt
+    while t < T + DOLFIN_EPS:
 
-    # Update boundary condition
-    p_exact.t = t
-    u_exact.t = t
-    f.t = t
+        # Update boundary condition
+        p_exact.t = t
+        u_exact.t = t
+        f.t = t
 
-    # Compute tentative velocity step
-    b1 = assemble(L1)
-    [bc.apply(A1, b1) for bc in bcu]
-    solve(A1, u1.vector(), b1, "bicgstab", "default")
+        # Compute tentative velocity step
+        b1 = assemble(L1)
+        [bc.apply(A1, b1) for bc in bcu]
+        solve(A1, u1.vector(), b1, "bicgstab", "default")
 
-    # Pressure correction
-    b2 = assemble(L2)
-    [bc.apply(A2, b2) for bc in bcp]
-    [bc.apply(p1.vector()) for bc in bcp]
-    solve(A2, p1.vector(), b2, "bicgstab", prec)
+        # Pressure correction
+        b2 = assemble(L2)
+        [bc.apply(A2, b2) for bc in bcp]
+        [bc.apply(p1.vector()) for bc in bcp]
+        solve(A2, p1.vector(), b2, "bicgstab", prec)
 
-    # Velocity correction
-    b3 = assemble(L3)
-    [bc.apply(A3, b3) for bc in bcu]
-    solve(A3, u1.vector(), b3, "bicgstab", "default")
+        # Velocity correction
+        b3 = assemble(L3)
+        [bc.apply(A3, b3) for bc in bcu]
+        solve(A3, u1.vector(), b3, "bicgstab", "default")
 
-    # Save to file
-    ufile << u1
-    pfile << p1
+        # Move to next time step
+        u0.assign(u1)
+        t += dt
 
-    # Move to next time step
-    u0.assign(u1)
-    t += dt
+    # Print errors
+    print("||u||_2: ", assemble(inner((u0-u_exact), (u0-u_exact))*dx))
+    print("||p||_2: ", assemble((p1-p_exact)*(p1-p_exact)*dx))
 
-# Print errors
-print("||u||_2: ", assemble(inner((u0-u_exact), (u0-u_exact))*dx))
-print("||p||_2: ", assemble((p1-p_exact)*(p1-p_exact)*dx))
 
+
+for i in range (4):
+    for j in range(4):
+        PrejectionSolve(pow(2,1+j), solutions[i])
