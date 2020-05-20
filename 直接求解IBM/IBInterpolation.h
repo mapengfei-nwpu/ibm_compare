f750c65cfe6d1d067b221d1a24ddb97ae0f0ce3c
mapengfei-nwpu <mapengfei@yahoo.com>
2020-05-21 00:37:52 +0800

    Update IBInterpolation.h
    have not finished paralell method.

--- 直接求解IBM/IBInterpolation.h
+++ 直接求解IBM/IBInterpolation.h
@@ -52,22 +52,6 @@ std::vector<std::array<double, 2>> get_global_dof_coordinates(const Function &f)
 	return dof_coordinates;
 }
 
-std::vector<double> get_global_dofs(const Function &f)
-{
-	/// some shorcut
-	auto mesh = f.function_space()->mesh();
-	auto mpi_comm = mesh->mpi_comm();
-	auto mpi_size = dolfin::MPI::size(MPI_COMM_WORLD);
-
-	/// get local values.
-	std::vector<double> local_values;
-	f.vector()->get_local(local_values);
-
-	/// collect local values on every process.
-	auto values = my_mpi_gather(local_values);
-	return values;
-}
-
 void get_gauss_rule(
 	const Function &f,
 	std::vector<double> &coordinates,
@@ -129,9 +113,10 @@ public:
 		/// Calculate global dof coordinates and dofs.
 		auto dof_coordinates = get_global_dof_coordinates(solid);
 		std::vector<double> values(dof_coordinates.size() * 2);
-
 		fluid_to_solid_raw(fluid, values, dof_coordinates);
+		/// 将属于本地的values取出并赋值。
 		solid.vector()->set_local(values);
+		solid.vector()->apply("insert");
 	}
 
 	void fluid_to_solid_raw(
@@ -143,15 +128,23 @@ public:
 		auto value_size = fluid.value_size();
 		for (size_t i = 0; i < solid_values.size() / value_size; i++)
 		{
+			/// TODO : 
+			/// 1. 寻找这个节点所在的单元 
+			/// Point point(2, solid_coordinates[i].data());
+			/// Cell cell = find_cell(point);
+			/// 2. 判断单元所在的节点
+			/// auto mpirank = um.global_map[cell.index()][]
 			Array<double> x(2, solid_coordinates[i].data());
 			Array<double> v(2);
 			fluid.eval(v, x);
 			for (size_t l = 0; l < value_size; l++)
 				solid_values[i * value_size + l] = v[l];
 		}
+		/// 因为固体的节点是不会重复的，因此
+		/// 所有节点上的 solid values 相加即可
 	}
 
-	/// Assign the solid displacement with the velocity of fluid.
+	
 	std::vector<double> solid_to_fluid(Function &fluid, Function &solid)
 	{
 		std::vector<double> solid_dof_coordinates;
@@ -182,12 +175,16 @@ public:
 		dolfin_assert(solid_values.size() == gauss_points.size());
 		dolfin_assert(solid_values.size() == gauss_weights.size());
 
+		/// TODO: 修改成PETSCVector
 		std::vector<double> results(fluid.function_space()->dim());
 
 		for (size_t i = 0; i < gauss_values.size(); i++)
 		{
+			Cell cell;
 			Point point(2, gauss_points[i].data());
-			Cell cell = find_cell_contianing_point(point, fluid);
+			/// 如果产生的单元不在本地，那么continue就行了。
+			if(!find_cell_contianing_point(point, fluid, cell))
+				continue;
 			auto dab = basis_values_gauss(point, cell, fluid);
 			integrate_basis(weights[i], dab, gauss_values[i], results);
 		}
@@ -216,7 +213,7 @@ public:
 			point.coordinates(),
 			coordinate_dofs.data(),
 			ufc_cell.orientation);
-		
+		/// 局部的dofmap就行了
 		auto cell_dofmap = f.function_space()->dofmap()->cell_dofs(cell.index());
 		std::vector<size_t> cell_dofmap_vector;
 		for (size_t i = 0; i < cell_dofmap.size(); i++)
@@ -233,7 +230,7 @@ public:
 		const std::array<double, 2> &solid_values,
 		std::vector<double> &results)
 	{
-
+		/// TODO:这些应该设为类的成员，可以直接调用
 		size_t space_dimension = 12;
 		size_t value_size = 2;
 
@@ -254,17 +251,31 @@ public:
 		}
 	}
 
-	Cell find_cell_contianing_point(const Point &point, const Function &f)
+	bool find_cell_contianing_point(const Point &point, const Function &f, Cell &cell)
 	{
-		/// must be regular grid.
-		auto mesh = f.function_space()->mesh();
+		/// TODO:这些应该设为类的成员，可以直接调用
+		size_t rank = 0;
+		auto mesh = um.mesh();
 
 		auto index_1 = 2 * um.hash(point);
 		auto index_2 = 2 * um.hash(point) + 1;
 
-		Cell cell_1(*mesh, index_1);
-		Cell cell_2(*mesh, index_2);
+		if(um.global_map[index_1][0] == rank){
+			Cell cell_1(*mesh, um.global_map[index_1][1]);
+			if(cell_1.contains(point)){
+				cell = cell_1;
+				return true;
+			}
+		}
+		
+		if(um.global_map[index_2][0] == rank){
+			Cell cell_2(*mesh, um.global_map[index_2][1]);
+			if(cell_2.contains(point)){
+				cell = cell_2;
+				return true;
+			}
+		}
 
-		return cell_1.contains(point) ? cell_1 : cell_2;
+		return false;
 	}
 };
