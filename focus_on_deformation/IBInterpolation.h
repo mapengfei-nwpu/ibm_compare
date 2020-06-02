2e3e103456bd4d41880ac5d6bd5051270b5ea904
mapengfei-nwpu <33214870+mapengfei-nwpu@users.noreply.github.com>
2020-06-02 10:59:18 +0800

    对位移的求导有问题。

---
+++ focus_on_deformation/IBInterpolation.h
@@ -0,0 +1,165 @@
+#include <dolfin.h>
+#include <dolfin/geometry/SimplexQuadrature.h>
+#include <numeric>
+using namespace dolfin;
+
+Cell find_cell(const Point &point, const IBMesh &um)
+{
+    auto mesh = um.mesh_ptr;
+
+    auto index_1 = 2 * um.hash(point);
+    auto index_2 = 2 * um.hash(point) + 1;
+    Cell cell_1(*mesh, index_1);
+    Cell cell_2(*mesh, index_2);
+
+    return cell_1.contains(point) ? cell_1 : cell_2;
+}
+
+void calculate_values_at_gauss_points(
+    const Function &displace,
+    std::vector<double> &weights,
+    std::vector<std::vector<double>> &points,
+    std::vector<std::vector<double>> &values)
+{
+    // Construct Gauss quadrature rules
+    // dimension 2 and order 9
+    SimplexQuadrature gq(2, 3);
+    auto mesh = displace.function_space()->mesh();
+    auto dofmap = displace.function_space()->dofmap();
+    auto element = displace.function_space()->element();
+    auto value_size = displace.value_size();
+    auto derivative_value_size = value_size * value_size;
+    auto space_dimension = element->space_dimension();
+
+    for (CellIterator cell(*mesh); !cell.end(); ++cell)
+    {
+        ufc::cell ufc_cell;
+        cell->get_cell_data(ufc_cell);
+
+        std::vector<double> coordinate_dofs;
+        cell->get_coordinate_dofs(coordinate_dofs);
+
+        std::vector<double> basis_derivative_values(value_size * space_dimension * 2);
+        
+        auto cell_dofmap = dofmap->cell_dofs(cell->index());
+
+        /// Compute quadrature rule for the cell.
+        /// qr.second and qr.first are the coordinate and weight of gauss point respectively.
+        auto qr = gq.compute_quadrature_rule(*cell);
+        for (size_t i = 0; i < qr.second.size(); i++)
+        {
+            std::vector<double> point({qr.first[2 * i], qr.first[2 * i + 1]});
+            element->evaluate_basis_derivatives_all(
+                1,
+                basis_derivative_values.data(),
+                point.data(),
+                coordinate_dofs.data(),
+                ufc_cell.orientation);
+            std::vector<double> derivative_value(derivative_value_size);
+            for (size_t k = 0; k < static_cast<size_t>(cell_dofmap.size()); k++)
+            {
+                for (size_t j = 0; j < derivative_value_size; j++)
+                {
+                    derivative_value[j] +=
+                        (*displace.vector())[cell_dofmap[k]] *
+                        basis_derivative_values[derivative_value_size * k + j];
+                }
+            }
+            {
+                double a = derivative_value[0];
+                double b = derivative_value[1];
+                double c = derivative_value[2];
+                double d = derivative_value[3];
+                
+                double det = 1.0/(a*d-b*c);
+                derivative_value[0] =  (b*b+d*d)/(det*det);
+                derivative_value[1] = -(c*d+a*b)/(det*det);
+                derivative_value[2] = -(c*d+a*b)/(det*det);
+                derivative_value[3] =  (a*a+c*c)/(det*det);
+            }
+
+            std::cout << derivative_value[0] 
+                      << ", " << derivative_value[1] 
+                      << ", " << derivative_value[2] 
+                      << ", " << derivative_value[3] 
+                      <<std::endl;
+
+            weights.push_back(qr.second[i]);
+            points.push_back(point);
+            values.push_back(derivative_value);
+        }
+    }
+}
+
+/// 一个函数离散成了三个变量：高斯点，函数值，权重。
+/// 现在要对它求积，将它与另一个函数空间的基函数点积，然后积分。
+
+/// 这个函数传入：一个高斯点，高斯点上的函数值，
+///        返回：这个单元内非零的基函数在这个点上的值，对应基函数的自由度索引。
+
+void calculate_basis_derivative_values(
+    const FunctionSpace &function_space,
+    const Cell &cell,
+    const std::vector<double> &point,
+    std::vector<size_t> &cell_dofmap,
+    std::vector<double> &cell_basis_derivatives)
+{
+    /// std::vector<size_t> &cell_dofmap 和 std::vector<double> &cell_basis_derivatives
+    /// 需要在调用这个函数之前分配好空间。
+
+    ufc::cell ufc_cell;
+    cell.get_cell_data(ufc_cell);
+
+    std::vector<double> coordinate_dofs;
+    cell.get_coordinate_dofs(coordinate_dofs);
+
+    /// store values derivative basis.
+    auto element = function_space.element();
+    element->evaluate_basis_derivatives_all(
+        1,
+        cell_basis_derivatives.data(),
+        point.data(),
+        coordinate_dofs.data(),
+        ufc_cell.orientation);
+    auto cell_dofmap_eigen = function_space.dofmap()->cell_dofs(cell.index());
+
+    for (size_t i = 0; i < cell_dofmap.size(); i++)
+    {
+        cell_dofmap[i]  = cell_dofmap_eigen[i];
+    }
+}
+
+std::vector<double> source_assemble(
+    const std::vector<double> &weights,
+    const std::vector<std::vector<double>> &points,
+    const std::vector<std::vector<double>> &values,
+    const FunctionSpace &function_space,
+    const IBMesh &um)
+{
+    std::vector<double> results(function_space.dim());
+    auto space_dimension = function_space.element()->space_dimension();
+
+    for (size_t i = 0; i < points.size(); i++)
+    {
+        auto point = points[i];
+        auto value = values[i];
+        auto weight = weights[i];
+
+        Point point_temp(2, point.data());
+        auto cell = find_cell(point_temp, um);
+
+        std::vector<size_t> cell_dofmap(space_dimension);
+        std::vector<double> cell_basis_derivatives(space_dimension * 4);
+
+        calculate_basis_derivative_values(function_space, cell, point, cell_dofmap, cell_basis_derivatives);
+
+        for (size_t j = 0; j < cell_dofmap.size(); j++)
+        {
+            results[cell_dofmap[j]] += weight * cell_basis_derivatives[j*4 + 0];// * value[k];
+            results[cell_dofmap[j]] += weight * cell_basis_derivatives[j*4 + 3];// * value[k];
+
+        }
+    }
+    return results;
+}
+
---
+++ focus_on_deformation/main.cpp
@@ -0,0 +1,260 @@
+#include "IBMesh.h"
+#include "IBInterpolation.h"
+
+#include <dolfin.h>
+#include "TentativeVelocity.h"
+#include "PressureUpdate.h"
+#include "VelocityUpdate.h"
+#include "Poisson.h"
+
+using namespace dolfin;
+
+// Define noslip domain
+class NoslipDomain : public SubDomain
+{
+    bool inside(const Array<double> &x, bool on_boundary) const
+    {
+        return near(x[0], 0) || near(x[0], 1.0) || near(x[1], 0);
+    }
+};
+
+// Define inflow domain
+class InflowDomain : public SubDomain
+{
+    bool inside(const Array<double> &x, bool on_boundary) const
+    {
+        return near(x[1], 1);
+    }
+};
+
+// Define inflow domain
+class Pinpoint : public SubDomain
+{
+    bool inside(const Array<double> &x, bool on_boundary) const
+    {
+        return near(x[1], 0) && near(x[0], 0);
+    }
+};
+
+// Define pressure boundary value at inflow
+class InflowVelocity : public Expression
+{
+public:
+    // Constructor
+    InflowVelocity() : Expression(2) {}
+
+    // Evaluate pressure at inflow
+    void eval(Array<double> &values, const Array<double> &x) const
+    {
+        values[0] = 1.0;
+        values[1] = 0.0;
+    }
+};
+class Position : public Expression
+{
+public:
+    // Constructor
+    Position() : Expression(2) {}
+
+    // Evaluate pressure at inflow
+    void eval(Array<double> &values, const Array<double> &x) const
+    {
+        values[0] = x[0];
+        values[1] = x[1];
+    }
+};
+
+void my_move(Mesh &mesh, Function &displacement)
+{
+    std::vector<double> vertex_values;
+    const std::size_t N = mesh.num_vertices();
+    displacement.compute_vertex_values(vertex_values);
+
+    // Move vertex coordinates
+    MeshGeometry &geometry = mesh.geometry();
+    std::vector<double> x(2);
+    for (std::size_t i = 0; i < N; i++)
+    {
+        for (std::size_t j = 0; j < 2; j++)
+            x[j] = vertex_values[j * N + i];
+        geometry.set(i, x.data());
+    }
+}
+
+int main()
+{
+    // Create chanel mesh
+    size_t nnn = 32;
+    Point point0(0, 0, 0);
+    Point point1(1.0, 1.0, 0);
+    IBMesh ba({point0, point1}, {nnn, nnn});
+
+    // Create circle mesh
+    auto circle = std::make_shared<Mesh>("./circle.xml.gz");
+    auto U = std::make_shared<Poisson::FunctionSpace>(circle);
+    auto body_velocity = std::make_shared<Function>(U);
+    auto body_position = std::make_shared<Function>(U);
+    auto body_disp = std::make_shared<Function>(U);
+
+    auto position = std::make_shared<Position>();
+    body_position->interpolate(*position);
+    body_disp->interpolate(*position);
+
+    // Create function spaces
+    auto V = std::make_shared<VelocityUpdate::FunctionSpace>(ba.mesh());
+    auto Q = std::make_shared<PressureUpdate::FunctionSpace>(ba.mesh());
+
+    // Set parameter values
+    double dt = 0.125 / static_cast<double>(nnn);
+    double T = 100;
+
+    // Define values for boundary conditions
+    auto v_in = std::make_shared<InflowVelocity>();
+    auto zero = std::make_shared<Constant>(0.0);
+    auto zero_vector = std::make_shared<Constant>(0.0, 0.0);
+
+    // Define subdomains for boundary conditions
+    auto noslip_domain = std::make_shared<NoslipDomain>();
+    auto inflow_domain = std::make_shared<InflowDomain>();
+    auto pinpoint_domain = std::make_shared<Pinpoint>();
+
+    // Define boundary conditions
+    DirichletBC noslip(V, zero_vector, noslip_domain);
+    DirichletBC inflow(V, v_in, inflow_domain);
+    DirichletBC pinpoint(Q, zero, pinpoint_domain, "pointwise");
+    std::vector<DirichletBC*> bcu = {{&inflow, &noslip}};
+    std::vector<DirichletBC*> bcp = {&pinpoint};
+
+    // Create functions
+    auto u0 = std::make_shared<Function>(V);
+    auto u1 = std::make_shared<Function>(V);
+    auto p0 = std::make_shared<Function>(Q);
+    auto p1 = std::make_shared<Function>(Q);
+    auto domain_force = std::make_shared<Function>(V);
+    
+    // Create coefficients
+    auto k = std::make_shared<Constant>(dt);
+    auto f = std::make_shared<Constant>(0, 0);
+
+    // Create forms
+    TentativeVelocity::BilinearForm a1(V, V);
+    TentativeVelocity::LinearForm L1(V);
+    PressureUpdate::BilinearForm a2(Q, Q);
+    PressureUpdate::LinearForm L2(Q);
+    VelocityUpdate::BilinearForm a3(V, V);
+    VelocityUpdate::LinearForm L3(V);
+
+    // Set coefficients
+    a1.k = k;
+    L1.k = k;
+    L1.u0 = u0;
+    L1.f = f;
+
+    L2.k = k;
+    L2.u1 = u1;
+
+    L3.k = k;
+    L3.u1 = u1;
+    L3.p1 = p1;
+
+    // Assemble matrices
+    Matrix A1, A2, A3;
+    assemble(A1, a1);
+    assemble(A2, a2);
+    assemble(A3, a3);
+
+    // Create vectors
+    Vector b1, b2, b3;
+
+  // Use amg preconditioner if available
+  const std::string prec(has_krylov_solver_preconditioner("amg") ? "amg" : "default");
+
+    // Create files for storing solution
+    File ufile("results/velocity.pvd");
+    File pfile("results/pressure.pvd");
+    File bfile("results/body.pvd");
+    File ffile("results/domain_force.pvd");
+
+
+    // Time-stepping
+    double t = dt;
+    std::vector<double> force(V->dim());
+
+    while (t < T + DOLFIN_EPS)
+    {
+        // Compute tentative velocity step
+        begin("Computing tentative velocity");
+        assemble(b1, L1);
+        /// apply the source term.
+        
+        for (size_t i = 0; i < b1.size(); i++)
+            b1.setitem(i, b1.getitem(i) - force[i]);
+        
+        for (std::size_t i = 0; i < bcu.size(); i++)
+            bcu[i]->apply(A1, b1);
+        
+        solve(A1, *u1->vector(), b1, "gmres", "default");
+        end();
+
+        // Pressure correction
+        begin("Computing pressure correction");
+        assemble(b2, L2);
+        for (std::size_t i = 0; i < bcp.size(); i++)
+        {
+            bcp[i]->apply(A2, b2);
+            bcp[i]->apply(*p1->vector());
+        }
+        solve(A2, *p1->vector(), b2, "bicgstab", prec);
+        end();
+
+        // Velocity correction
+        begin("Computing velocity correction");
+        assemble(b3, L3);
+        for (std::size_t i = 0; i < bcu.size(); i++)
+            bcu[i]->apply(A3, b3);
+        solve(A3, *u1->vector(), b3, "gmres", "default");
+        end();
+
+        // force calculation
+        begin("Computing elastic force");
+        end();
+        
+        /// 1. 进行速度插值
+        body_velocity->interpolate(*u1);
+
+        /// 2. 更新坐标 
+        for (size_t i = 0; i < body_disp->vector()->size(); i++)
+        {
+            double disp = body_disp->vector()->getitem(i) + dt*body_velocity->vector()->getitem(i);
+            body_disp->vector()->setitem(i, disp);
+        }
+        
+        /// 3. 移动网格
+        my_move(*circle, *body_disp);
+
+        /// 4. 计算出实时坐标下的高斯点、高斯权重、高斯点上的 P = mu*F*F^{T}
+        std::vector<double> weights; /// shape = (1,)
+        std::vector<std::vector<double>> points; /// shape = (2,)
+        std::vector<std::vector<double>> values; /// shape = (2,2)
+        calculate_values_at_gauss_points(*body_disp, weights, points, values);
+        
+
+        /// 5. 组装 \int mu*FF^{T}:grad(v) dx
+        force = source_assemble(weights, points, values, *(u1->function_space()), ba);
+       
+        // Save to file
+        domain_force->vector()->set_local(force);
+        ffile << *domain_force;
+        ufile << *u1;
+        pfile << *p1;
+        bfile << *body_disp;
+
+        // Move to next time step
+        *u0 = *u1;
+        *p0 = *p1;
+        t += dt;
+        cout << "t = " << t << endl;
+    }
+
+    return 0;
+}
