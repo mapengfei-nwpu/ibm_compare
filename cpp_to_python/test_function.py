398352d0a7f06d18fd473dda94f396d5c9f4c948
mapengfei-nwpu <33214870+mapengfei-nwpu@users.noreply.github.com>
2020-06-03 14:48:08 +0800

    测试成功
    在test_function中，用_cpp_object替代cpp_object就成功了。

--- cpp_to_python/test_function.py
+++ cpp_to_python/test_function.py
@@ -5,13 +5,8 @@ import matplotlib.pyplot as plt
 
 mesh = RectangleMesh(Point(0, 0), Point(1, 1), 10, 10)
 V = VectorFunctionSpace(mesh, "P", 1)
-T = TensorFunctionSpace(mesh, "P", 1)
-
-position = Expression(("x[0] + x[0]*x[0]", "x[1] + x[1]*x[1]"), degree = 2)
-displace = Expression(("x[0]*x[0]", "x[1]*x[1]"), degree = 2)
-
 u0 = Function(V)
 
 import test_function
 
-test_function.size(u0)
+test_function.size(u0._cpp_object)
