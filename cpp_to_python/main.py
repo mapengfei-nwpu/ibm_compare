d2ae32a1693598e26ad04bcbfd4783f0460f814f
mapengfei-nwpu <33214870+mapengfei-nwpu@users.noreply.github.com>
2020-06-08 14:14:30 +0800

    Update main.py
    写完了。感觉不对。

--- cpp_to_python/main.py
+++ cpp_to_python/main.py
@@ -1,23 +1,42 @@
 from dolfin import *
+from mshr import *
+
+from IBInterpolation import *
+from IBMesh import *
+
 import matplotlib.pyplot as plt
 
-# Define mesh
-mesh = RectangleMesh(Point(0, 0), Point(1, 1), 16, 16)
+# Define fluid_mesh
+points = [Point(0, 0, 0), Point(1, 1, 0)]
+seperations = [32, 32, 0]
+fluid_mesh = IBMesh(points, seperations)
+solid_mesh = generate_mesh(Circle(Point(0.6, 0.5), 0.2), 10)
 
-# Define function spaces W
-P2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
-P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+# Define function spaces W for fluid
+P2 = VectorElement("Lagrange", fluid_mesh.ufl_cell(), 2)
+P1 = FiniteElement("Lagrange", fluid_mesh.ufl_cell(), 1)
 TH = P2 * P1
-W = FunctionSpace(mesh, TH)
+W = FunctionSpace(fluid_mesh, TH)
 
-# Define trial and test functions
+# Define function spaces W for solid
+Vs = VectorFunctionSpace(solid_mesh, "Lagrange", 2)
+
+# Define interpolation object
+IB = IBInterpolation(fluid_mesh)
+
+# Define trial and test functions for fluid
 (u, p) = TrialFunctions(W)
 (v, q) = TestFunctions(W)
 
+# Define trial and test functions for solid
+us = TrialFunctions(Vs)
+vs = TestFunctions(Vs)
+
 # Set parameter values
 dt = 0.01
-T = 100
+T = 10
 nu = 0.01
+nu_s = 0.1
 
 # Define boundary conditions
 noslip = DirichletBC(W.sub(0), (0, 0),"near(x[0],1) || near(x[0],0) || near(x[1],0)")
@@ -26,18 +45,29 @@ pinpoint = DirichletBC(W.sub(1), 0, "near(x[0],0) && near(x[1],0)", "pointwise")
 bcu = [noslip, upflow]
 bcp = [pinpoint]
 
-
-# Create functions
+# Create functions for fluid
 w = Function(W)
 (un, pn) = w.split()
+(f, _) = Function(W).split(True)
 
-# Define variational problem
-F1 = (1/dt)*inner((u-un), v)*dx + inner(grad(un)*un, v)*dx + nu*(inner(grad(u), grad(v)) - div(v)*p + q*div(u))*dx
+# Create functions for solid
+velocity = Function(Vs)
+disp = Function(Vs)
+force = Function(Vs)
+
+# Define variational problem for fluid
+F1 = (1/dt)*inner((u-un), v)*dx + inner(grad(un)*un, v)*dx + nu*(inner(grad(u), grad(v)) - div(v)*p + q*div(u))*dx + inner(f, v)*dx
 a1 = lhs(F1)
 L1 = rhs(F1)
 
+# Define variational problem for solid
+F2 = nu_s*inner(grad(disp), grad(vs))*dx + inner(us, vs)*dx
+a2 = lhs(F2)
+L2 = rhs(F2)
+
 # Assemble matrices
 A1 = assemble(a1)
+A2 = assemble(a2)
 
 # Use amg preconditioner if available
 prec = "amg" if has_krylov_solver_preconditioner("amg") else "default"
@@ -48,6 +78,8 @@ parameters['krylov_solver']['nonzero_initial_guess'] = True
 # Create files for storing solution
 ufile = File("results/velocity.pvd")
 pfile = File("results/pressure.pvd")
+ffile = File("results/force.pvd")
+
 # Time-stepping
 t = dt
 w_ = Function(W)
@@ -57,11 +89,19 @@ while t < T + DOLFIN_EPS:
     [bc.apply(A1, b1) for bc in bcu]
     solve(A1, w_.vector(), b1)
     (u_, p_) = w_.split(True)
+    # interpolate velocity from fluid to solid
+    IB.fluid_to_solid(u_._cpp_object, velocity._cpp_object)
+    disp.vector()[:] = velocity.vector()[:]*dt + disp.vector()[:]
+    b2 = assemble(L2)
+    solve(A2, force.vector(), b2)
+    IB.solid_to_fluid(f._cpp_object, force._cpp_object)
+    # interpolate force from solid to fluid
     un.assign(u_)
     pn.assign(p_)
     # Save to file
     ufile << un
     pfile << pn
+    ffile << force
     t += dt
     print(t)
 
